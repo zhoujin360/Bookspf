68aaff2a604bb803613d6d2a831ca7efa1f9e302
CShisan <510696435@qq.com>
2020-05-07 17:59:25 +0800

    start

---
+++ src/main/java/cn/Bookspf/BookspfApplication.java
@@ -0,0 +1,13 @@
+package cn.Bookspf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookspfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookspfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/Bookspf/BookspfApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.Bookspf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookspfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
