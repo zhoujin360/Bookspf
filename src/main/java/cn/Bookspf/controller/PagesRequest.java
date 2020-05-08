d27743e1ad01705970fc7481afb3502d3a91cc15
CShisan <510696435@qq.com>
2020-05-08 18:10:24 +0800

    页面start

---
+++ src/main/java/cn/Bookspf/controller/PagesRequest.java
@@ -0,0 +1,30 @@
+package cn.Bookspf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PagesRequest {
+	
+	//主页
+	@RequestMapping({"/","/index"})
+	public String index (Model model) {
+		
+		return "index";
+	}
+	
+	//注册页
+	@RequestMapping("/register")
+	public String register (Model model) {
+		
+		return "register";
+	}
+	
+	//注册页
+	@RequestMapping("/login")
+	public String login (Model model) {
+			
+		return "login";
+	}
+}
