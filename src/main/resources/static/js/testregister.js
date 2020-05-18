53a94c8a473ac10ec7715e1874a8a8241ddb8794
zy-16688 <59195081+zy-16688@users.noreply.github.com>
2020-05-19 00:21:56 +0800

    1
    1

--- src/main/resources/static/js/testregister.js
+++ src/main/resources/static/js/testregister.js
@@ -1,51 +1,58 @@
-//校验用户名
-function checkUsername() {
-    var username = $("#reg_username").val();
-    var reg_username = /^\w{3,10}$/
-    var flag = reg_username.test(username);
-    if (flag) { //若校验通过
-        $("#reg_username").css("border", "");
-    } else {
-        $("#reg_username").css("border", "1px solid red");
+window.addEventListener("load", function () {
+    var username = document.querySelector("#reg_username");
+    var password = document.querySelector("#reg_password");
+    var email = document.querySelector("#email");
+    var regForm = document.querySelector("#regForm");
+    //校验用户名
+    function checkUsername() {
+        var reg_username = /^\w{3,10}$/
+        var flag = reg_username.test(username.value);
+        if (flag) { //若校验通过
+            username.style.border = "";
+        } else {
+            username.style.border = "1px solid red";
+        }
+        return flag;
     }
-    return flag;
-}
-//校验密码
-function checkPassword() {
-    var password = $("#reg_password").val();
-    var reg_password = /^\w{6,20}$/
-    var flag = reg_password.test(password);
-    if (flag) { //若校验通过
-        $("#reg_password").css("border", "");
-    } else {
-        $("#reg_password").css("border", "1px solid red");
+    //校验密码
+    function checkPassowrd() {
+        var reg_password = /^\w{6,20}$/;
+        var flag = reg_password.test(password.value);
+        if (flag) {
+            password.style.border = "";
+        } else {
+            password.style.border = "1px solid red";
+        }
+        return flag;
     }
-    return flag;
-}
-//校验邮箱
-function checkEmail() {
-    var email = $("#email").val();
-    var reg_email = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    var flag = reg_email.test(email);
-    if (flag) { //若校验通过
-        $("#eamil").css("border", "");
-    } else {
-        $("#email").css("border", "1px solid red");
+    //校验邮箱
+    function checkEmail() {
+        var reg_email = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        var flag = reg_email.test(email.value);
+        if (flag) { //若校验通过
+            email.style.border = "";
+        } else {
+            email.style.border = "1px solid red";
+        }
+        return flag;
     }
-    return flag;
-}
 
+    // regForm.onsumbit = function () {
+    //     return checkEmail() && checkPassword() && checkPhone() && checkUsername();
+    // }
 
-$(function() {
-    //若校验不通过无法提交
-    $("#regForm").submit(function() {
-        return checkEmail() && checkPassword() && checkPhone() && checkUsername()
-    })
-    $("#reg_usernmae").blur(checkUsername);
-    $("#reg_password").blur(checkPassword);
-    $("#email").blur(checkEmail);
+    username.onblur = function () {
+        checkUsername();
+    }
+
+    password.onblur = function () {
+        checkPassword();
+    }
+
+    email.onblur = function () {
+        checkEmail();
+    }
 })
-
 
 var register = new Vue({
     el: "#regForm",
@@ -55,13 +62,13 @@ var register = new Vue({
         email: ""
     },
     methods: {
-        submit: function() {
+        submit: function () {
             var that = this;
             axios.post("/register", {
                 username: that.username,
                 password: that.password,
                 email: that.email
-            }).then(function(response) {
+            }).then(function (response) {
                 console.log(response);
             })
         }
