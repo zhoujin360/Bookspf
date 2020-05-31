273c0842a66d799aa75f16d28147db9400a8be63
CShisan <55305718+CShisan@users.noreply.github.com>
2020-05-31 17:32:05 +0800

    1

--- src/main/java/cn/Bookspf/model/RO/ShopcarResponse.java
+++ src/main/java/cn/Bookspf/model/RO/ShopcarResponse.java
@@ -3,19 +3,25 @@ package cn.Bookspf.model.RO;
 import java.util.ArrayList;
 
 import cn.Bookspf.model.DO.DBShopcar;
+import cn.Bookspf.model.DTO.ShopcarStatistics;
 
 public class ShopcarResponse extends Response{
 	private ArrayList<DBShopcar> shopcars;
+	private ArrayList<ShopcarStatistics> shopcarsinfo;
 	
 	public ShopcarResponse() {}
-	public ShopcarResponse(ArrayList<DBShopcar> shopcars) {
-		this.shopcars=shopcars;
-	}
 	public ArrayList<DBShopcar> getShopcars() {
 		return shopcars;
 	}
 	public void setShopcars(ArrayList<DBShopcar> shopcars) {
 		this.shopcars = shopcars;
 	}
-	
+
+	public ArrayList<ShopcarStatistics> getShopcarsinfo() {
+		return shopcarsinfo;
+	}
+
+	public void setShopcarsinfo(ArrayList<ShopcarStatistics> shopcarsinfo) {
+		this.shopcarsinfo = shopcarsinfo;
+	}
 }
