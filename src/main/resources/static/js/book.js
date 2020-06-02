67728eb31e1d33304effee314ca0fd08dba9b902
CShisan <55305718+CShisan@users.noreply.github.com>
2020-06-02 18:02:58 +0800

    1

--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -10,3 +10,37 @@ var top = new Vue({
         }
     }
 });
+
+function noLogin() {
+    alert("请登录再操作")
+}
+
+function changeBid(bid) {
+    app.bid = bid;
+}
+
+
+var app = new Vue({
+    el: "#box",
+    data: {
+        bid: ""
+    },
+    methods: {
+        addShopcar: function() {
+            var that = this;
+            axios.post("/addShopcar", {
+                bid: that.bid
+            }).then(function(response) {
+                alert(response.data.mes);
+            })
+        },
+        buy: function() {
+            var that = this;
+            axios.post("/buyBook", {
+                bid: that.bid
+            }).then(function(response) {
+                alert(response.data.mes);
+            })
+        }
+    }
+})
