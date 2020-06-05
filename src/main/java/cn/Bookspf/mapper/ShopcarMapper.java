23bfb239369932a2be94d515908984fd394d9d48
CShisan <55305718+CShisan@users.noreply.github.com>
2020-06-05 15:39:18 +0800

    购物车bug

--- src/main/java/cn/Bookspf/mapper/ShopcarMapper.java
+++ src/main/java/cn/Bookspf/mapper/ShopcarMapper.java
@@ -14,6 +14,14 @@ public interface ShopcarMapper {
     @Insert("insert into shopcar(carid,uid,bid,booknumber) values(#{carid},#{uid},#{bid},#{booknumber})")
     public void insertShopcar(Long carid,Integer uid,Integer bid,Integer booknumber);
 
+    //获得记录的某书数量
+    @Select("select booknumber from shopcar where carid=#{carid} and bid=#{bid}")
+    public Integer getBooknumber(Long carid,Integer bid);
+
+    //更新购物车
+    @Update("update shopcar set booknumber=#{booknumber} where carid=#{carid} and bid=#{bid}")
+    public void updateShopcar(Long carid,Integer bid,Integer booknumber);
+
     //根据购物车id查询所有
     @Select("select * from shopcar where carid=#{carid}")
     public ArrayList<DBShopcar> getShopcarOfCarid(Long carid);
@@ -21,6 +29,14 @@ public interface ShopcarMapper {
     //查询某用户的购物车
     @Select("select * from shopcar where uid=#{uid}")
     public ArrayList<DBShopcar> getShopcarOfUid(Integer uid);
+
+    //查询某用户购物车id
+    @Select("select distinct carid from shopcar where uid=#{uid}")
+    public Long getShopcaridOfUid(Integer uid);
+
+    //查询用户购物车是否存在某书
+    @Select("select bid from shopcar where carid=#{carid} and bid=#{bid}")
+    public Integer findBid(Long carid,Integer bid);
 
     //删除某用户整个购物车
     @Delete("delete from shopcar where uid=#{uid}")
